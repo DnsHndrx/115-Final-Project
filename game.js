58552fd2e9a6350adfec7ae5eda8619837a49bee
awork211 <work.aaron89@gmail.com>
2021-11-09 08:36:42 -0500

    game display

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 var playerChoice = "rock";
 var aiChoice = null;
 
@@ -5,36 +6,61 @@ var aiOptions = [ "rock",
 "paper",
 "scissors"];
 
-var index = Math.floor(Math.random() * aiOptions.length);
-
-if (aiOptions[index] == "rock" && playerChoice == "paper") {
-    aiChoice = "Yes! You won! " + aiOptions[index], + playerChoice;
-}
-if (aiOptions[index] == "paper" && playerChoice == "scissors") {
-    aiChoice = "Yes! You won! " + aiOptions[index], + playerChoice;
-}
-if (aiOptions[index] == "scissors" && playerChoice == "rock") {
-    aiChoice = "Yes! You won! " + aiOptions[index], + playerChoice;
-} 
-if (aiOptions[index] == "scissors" && playerChoice == "paper") {
-    aiChoice = "Sorry. You lose " + aiOptions[index], + playerChoice;
-}
-if (aiOptions[index] == "paper" && playerChoice == "rock") {
-    aiChoice = "Sorry. You lose " + aiOptions[index], + playerChoice;
-}
-if (aiOptions[index] == "rock" && playerChoice == "rock") {
-    aiChoice = "its a draw " + aiOptions[index], + playerChoice;
-}
-if (aiOptions[index] == "paper" && playerChoice == "paper") {
-    aiChoice = "its a draw " + aiOptions[index], + playerChoice;
-}
-if (aiOptions[index] == "scissors" && playerChoice == "scissors") {
-    aiChoice = "its a draw " + aiOptions[index], + playerChoice;
-} 
-
-alert(aiChoice)
-
 var icon = document.querySelectorAll('i');
+var p = document.getElementById("message")
+
+var index = Math.floor(Math.random() * aiOptions.length);
+function play(){
+    for(let i = 3; i < 6; i++){
+        if(icon[i].classList.contains(aiOptions[index]) == false){
+            console.log(aiOptions[index])
+            icon[i].style.visibility = 'hidden'
+        }
+    }
+    if (aiOptions[index] == "rock" && playerChoice == "paper") {
+        aiChoice = "Yes! You won! " + aiOptions[index], + playerChoice;
+    }
+    if (aiOptions[index] == "paper" && playerChoice == "scissors") {
+        aiChoice = "Yes! You won! " + aiOptions[index], + playerChoice;
+    }
+    if (aiOptions[index] == "scissors" && playerChoice == "rock") {
+        aiChoice = "Yes! You won! " + aiOptions[index], + playerChoice;
+    } 
+    if (aiOptions[index] == "scissors" && playerChoice == "paper") {
+        aiChoice = "Sorry. You lose " + aiOptions[index], + playerChoice;
+    }
+    if (aiOptions[index] == "paper" && playerChoice == "rock") {
+        aiChoice = "Sorry. You lose " + aiOptions[index], + playerChoice;
+    }
+    if (aiOptions[index] == "rock" && playerChoice == "rock") {
+        aiChoice = "its a draw " + aiOptions[index], + playerChoice;
+    }
+    if (aiOptions[index] == "paper" && playerChoice == "paper") {
+        aiChoice = "its a draw " + aiOptions[index], + playerChoice;
+    }
+    if (aiOptions[index] == "scissors" && playerChoice == "scissors") {
+        aiChoice = "its a draw " + aiOptions[index], + playerChoice;
+    }
+    message.innerHTML = aiChoice
+    
+    console.log(aiOptions[index])
+    console.log(aiChoice)
+}
+
+function playerChooses(element){
+    if(element.matches('.clicked.rock')){
+        playerChoice = 'rock';
+        console.log('this is rock move')
+    } else if(element.matches('.clicked.paper')){
+        playerChoice = 'paper'
+        console.log('this is paper move')
+    } else {
+        playerChoice = 'scissors'
+        console.log('this is scissors move')
+    }
+}
+
+console.log(playerChoice)
 
 function changeColor(element){
     element.classList.add('clicked')
