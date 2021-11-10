69309826f91d1e648b71defc35b9236db9f8f4c1
awork211 <work.aaron89@gmail.com>
2021-11-10 08:35:38 -0500

    storage to profile

--- signUp.js
+++ signUp.js
@@ -3,7 +3,6 @@ let user_email = document.getElementById('email')
 let btn = document.getElementsByTagName('button')[0]
 
 btn.addEventListener("click", function(){
-    localStorage.clear()
     localStorage.username = user_name.value
     localStorage.email = user_email.value
     //counters
